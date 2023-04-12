9cfe33b6b61ff2a74101e0423e8344e29b0f5f79
Scott Cowie <sacowie@gmail.com>
2023-04-12 14:34:39 +0100

    Add UserData and cURL examples

---
+++ userdata_examples/Windows-DeployIIS.ps1
@@ -0,0 +1,4 @@
+<powershell>
+Install-WindowsFeature -name Web-Server -IncludeManagementTools
+New-Item -Path C:\inetpub\wwwroot\index.html -ItemType File -Value "AWS is a wonderful cloud platform!" -Force
+</powershell>
